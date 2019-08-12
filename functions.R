72c4f715f23d52693f97ca0f21e8ed5450bac6de
Tylar Murray <murray.tylar@gmail.com>
2019-08-12 16:21:29 +0000

    editor autoformat

--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
-library(tidyverse) 
-library(here) 
-library(glue) 
+library(tidyverse)
+library(here)
+library(glue)
 library(rerddap)  # install.packages("rerddap")
 library(raster)
 library(leaflet)
@@ -15,7 +15,7 @@ grid_to_raster <- function (grid, var) {
   # original: plotdap:::get_raster
   # grid <- sst_grid
   #library(magrittr)
-  
+
   times <- grid$summary$dim$time$vals
   lats <- grid$summary$dim$latitude$vals
   lons <- grid$summary$dim$longitude$vals
@@ -24,14 +24,14 @@ grid_to_raster <- function (grid, var) {
   ext <- raster::extent(xlim[1], xlim[2], ylim[1], ylim[2])
   r <- if (length(times) > 1) {
     d <- dplyr::arrange(grid$data, time, desc(lat), lon)
-    b <- raster::brick(nl = length(times), nrows = length(lats), 
+    b <- raster::brick(nl = length(times), nrows = length(lats),
                        ncols = length(lons))
     raster::values(b) <- lazyeval::f_eval(var, d)
     raster::setExtent(b, ext)
   }
   else {
     d <- dplyr::arrange(grid$data, desc(lat), lon)
-    r <- raster::raster(nrows = length(lats), ncols = length(lons), 
+    r <- raster::raster(nrows = length(lats), ncols = length(lons),
                         #ext = ext, vals = lazyeval::f_eval(var, d)) # plotdap:::get_raster
                         ext = ext, vals = d[,var])
   }
@@ -41,26 +41,26 @@ grid_to_raster <- function (grid, var) {
 }
 
 get_dates <- function(info){
-  info$alldata$time %>% 
-    filter(attribute_name=="actual_range") %>% 
-    pull(value) %>% 
-    str_split(", ", simplify = T) %>% 
-    as.numeric() %>% 
+  info$alldata$time %>%
+    filter(attribute_name=="actual_range") %>%
+    pull(value) %>%
+    str_split(", ", simplify = T) %>%
+    as.numeric() %>%
     as.POSIXct(origin = "1970-01-01", tz = "GMT")
 }
 
 get_box <- function(lon, lat, cells_wide){
   w <- cells_wide * 0.01 / 2
   box <- list(
-    lon = c(round(lon, 2) - w, round(lon, 2) + w), 
+    lon = c(round(lon, 2) - w, round(lon, 2) + w),
     lat = c(round(lat, 2) - w, round(lat, 2) + w))
 }
 
 get_raster <- function(info, lon, lat, date="last", field="sst"){
   g <- griddap(
-    info, longitude = lon, latitude = lat, 
+    info, longitude = lon, latitude = lat,
     time = c(date, date), fields = field)
-  grid_to_raster(g, "sst") %>% 
+  grid_to_raster(g, "sst") %>%
     leaflet::projectRasterForLeaflet(method="ngb")
 
 }
@@ -74,7 +74,7 @@ map_raster <- function(r, site_lon, site_lat, site_label, title){
     #addProviderTiles(providers$Stamen.TonerLabels) %>%
     addRasterImage(r, colors = pal, opacity = 0.8, project=F, group="CHL") %>%
     addMarkers(lng = site_lon, lat = site_lat, label = site_label) %>%
-    addLegend(pal = pal, values = values(r), title = title, position="bottomright") %>% 
+    addLegend(pal = pal, values = values(r), title = title, position="bottomright") %>%
     addLayersControl(
       baseGroups = c("Color", "B&W"),
       overlayGroups = c("SST"),
@@ -83,9 +83,9 @@ map_raster <- function(r, site_lon, site_lat, site_label, title){
 
 get_raster_2 <- function(info, lon, lat, date="last", field_2="chl"){
   g_2 <- griddap(
-    info, longitude = lon, latitude = lat, 
+    info, longitude = lon, latitude = lat,
     time = c(date, date), fields = field_2)
-  grid_to_raster(g_2, "chl") %>% 
+  grid_to_raster(g_2, "chl") %>%
     leaflet::projectRasterForLeaflet(method="ngb")
 }
 
@@ -98,7 +98,7 @@ map_raster_2 <- function(r, site_lon, site_lat, site_label, title){
     #addProviderTiles(providers$Stamen.TonerLabels) %>%
     addRasterImage(r, colors = pal, opacity = 0.8, project=F, group="CHL") %>%
     addMarkers(lng = site_lon, lat = site_lat, label = site_label) %>%
-    addLegend(pal = pal, values = values(r), title = title, position="bottomright") %>% 
+    addLegend(pal = pal, values = values(r), title = title, position="bottomright") %>%
     addLayersControl(
       baseGroups = c("Color", "B&W"),
       overlayGroups = c("CHL"),
@@ -106,104 +106,104 @@ map_raster_2 <- function(r, site_lon, site_lat, site_label, title){
 }
 
 get_timeseries <- function(info, lon, lat, csv, field="sst"){
-  
+
   dates  <- get_dates(info)
-  
+
   if (file.exists(csv)){
-    d_prev <- read_csv(csv) %>% 
+    d_prev <- read_csv(csv) %>%
       arrange(date)
-    start_date <- read_csv(csv) %>% 
-      tail(1) %>% 
-      pull(date) %>% 
+    start_date <- read_csv(csv) %>%
+      tail(1) %>%
+      pull(date) %>%
       as.POSIXct()
   } else {
     start_date <- dates[1]
   }
-  
+
   v <- griddap(
     info,
-    longitude = c(lon, lon), latitude = c(lat, lat), 
+    longitude = c(lon, lon), latitude = c(lat, lat),
     time = c(start_date, dates[2]), fields = field)
-  
+
   d_now <- v$data %>%
     as_tibble() %>%
     mutate(
       date = lubridate::as_date(time, "%Y-%m-%dT00:00:00Z")) %>%
     select(date, sst) %>%
     arrange(date)
-  
+
   if (file.exists(csv)){
-    d <- bind_rows(d_prev, d_now) %>% 
+    d <- bind_rows(d_prev, d_now) %>%
       filter(!duplicated(date))
   } else {
     d <- d_now
   }
-  
-  d %>% 
+
+  d %>%
     write_csv(csv)
   d
 }
 
 get_timeseries_2 <- function(info, lon, lat, csv, field="chl"){
-  
+
   dates_2  <- get_dates(info)
-  
+
   if (file.exists(csv)){
-    d_prev <- read_csv(csv) %>% 
+    d_prev <- read_csv(csv) %>%
       arrange(date)
-    start_date <- read_csv(csv) %>% 
-      tail(1) %>% 
-      pull(date) %>% 
+    start_date <- read_csv(csv) %>%
+      tail(1) %>%
+      pull(date) %>%
       as.POSIXct()
   } else {
     start_date <- dates[1]
   }
-  
+
   v_2 <- griddap(
     info,
-    longitude = c(lon, lon), latitude = c(lat, lat), 
+    longitude = c(lon, lon), latitude = c(lat, lat),
     time = c(start_date, dates[2]), fields = field)
-  
+
   d_now <- v$data %>%
     as_tibble() %>%
     mutate(
       date = lubridate::as_date(time, "%Y-%m-%dT00:00:00Z")) %>%
     select(date, chl) %>%
     arrange(date)
-  
+
   if (file.exists(csv)){
-    d_2 <- bind_rows(d_prev, d_now) %>% 
+    d_2 <- bind_rows(d_prev, d_now) %>%
       filter(!duplicated(date))
   } else {
     d_2 <- d_now
   }
-  
-  d_2 %>% 
+
+  d_2 %>%
     write_csv(csv)
   d_2
 }
 
 plot_timeseries <- function(d, title="SST", color="red", dyRangeSelector=T, ...){
-  p <- xts(select(d, -date), order.by=d$date) %>% 
+  p <- xts(select(d, -date), order.by=d$date) %>%
     dygraph(main=title, ...) %>%
     dyOptions(
       colors = color,
-      fillGraph = TRUE, fillAlpha = 0.4) 
+      fillGraph = TRUE, fillAlpha = 0.4)
   if (dyRangeSelector){
-    p <- p %>% 
+    p <- p %>%
       dyRangeSelector()
   }
   p
 }
 
 plot_timeseries_2 <- function(d_2, title="CHL", color="red", dyRangeSelector=T, ...){
-  p_2 <- xts(select(d_2, -date), order.by=d$date) %>% 
+  p_2 <- xts(select(d_2, -date), order.by=d$date) %>%
     dygraph(main=title, ...) %>%
     dyOptions(
       colors = color,
-      fillGraph = TRUE, fillAlpha = 0.4) 
+      fillGraph = TRUE, fillAlpha = 0.4)
   if (dyRangeSelector){
-    p_2 <- p_2 %>% 
+    p_2 <- p_2 %>%
       dyRangeSelector()
   }
   p_2
@@ -211,11 +211,11 @@ plot_timeseries_2 <- function(d_2, title="CHL", color="red", dyRangeSelector=T, 
 
 popup_site_sst <- function(site_id, ...){
   csv <- here(glue("data/sst/sst_{site$id}.csv"))
-  site <- read_csv(here("data/sites.csv")) %>% 
+  site <- read_csv(here("data/sites.csv")) %>%
     filter(id == params$site_id)
-  
+
   sst <- info('jplMURSST41mday')
-  
+
   d   <- get_timeseries(sst, lon=site$lon, lat=site$lat, csv=csv, field="sst")
   p <- plot_timeseries(d, title="SST", color="red", dyRangeSelector=F, ...)
   #p
@@ -224,14 +224,13 @@ popup_site_sst <- function(site_id, ...){
 
 popup_site_chl <- function(site_id, ...){
   csv_chl <- here(glue("data/chl/chl_{site$id}.csv"))
-  site_chl <- read_csv(here("data/sites.csv")) %>% 
+  site_chl <- read_csv(here("data/sites.csv")) %>%
     filter(id == params$site_id)
-  
+
   chl <- info("nesdisVHNSQchlaMonthly")
-  
+
   d_2   <- get_timeseries(chl, lon=site$lon, lat=site$lat, csv=csv, field="chl")
   p_2 <- plot_timeseries(d_2, title="CHL", color="red", dyRangeSelector=F, ...)
   #p
   as.character(p_2)
 }
-
